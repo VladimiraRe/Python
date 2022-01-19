d85cabb033a342ad7f08e6c9390c6d6454517f7f
Mira <vladimira_ra@mail.ru>
2022-01-19 20:24:55 +0300

    Add files

---
+++ 19_turtle_game/19.2_turtle-race.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+import random
+
+is_race_on = False
+screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Witch turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_position = [-125, -75, -25, 25, 75, 125]
+all_turtles = []
+
+for i in range(6):
+    new_turtle = Turtle(shape="turtle")
+    new_turtle.penup()
+    new_turtle.color(colors[i])
+    new_turtle.goto(x=-230, y=y_position[i])
+    new_turtle.color(colors[i])
+    all_turtles.append(new_turtle)
+
+if user_bet:
+    is_race_on = True
+
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() > 230:
+            is_race_on = False
+            winner = turtle.pencolor()
+            if winner == user_bet:
+                print(f"You've won! The {winner} turtle is the winner.")
+            else:
+                print(f"You lose. The {winner} turtle is the winner.")
+        turtle.forward(random.randint(0, 10))
+
+
+
+
+
+
+# screen.exitonclick()
